fcae162a14bb88c29c8b3c7c346727bc6007b0d6
Andre <andrevitorlopes@gmail.com>
2024-02-13 15:03:46 +0100

    Add s3 notification

--- iac/s3.tf
+++ iac/s3.tf
@@ -2,4 +2,11 @@ resource "aws_s3_bucket" "my-app-images" {
   bucket = "my-super-app-images"
 }
 
+resource "aws_s3_bucket_notification" "images_put_notification" {
+  bucket = aws_s3_bucket.my-app-images.id
 
+  topic {
+    topic_arn = aws_sns_topic.topic.arn
+    events    = ["s3:ObjectCreated:*"]
+  }
+}
--- iac/sqs_sns.tf
+++ iac/sqs_sns.tf
@@ -4,7 +4,8 @@ resource "aws_sqs_queue" "queue" {
 }
 
 resource "aws_sns_topic" "topic" {
-  name = "image-events"
+  name   = "image-events"
+  policy = data.aws_iam_policy_document.sns-topic-policy.json
 }
 
 resource "aws_sns_topic_subscription" "topic_subscription" {
@@ -40,6 +41,38 @@ data "aws_iam_policy_document" "sqs-queue-policy" {
 
       values = [
         aws_sns_topic.topic.arn
+      ]
+    }
+  }
+}
+
+
+data "aws_iam_policy_document" "sns-topic-policy" {
+  policy_id = "arn:aws:sns:${var.region}:${var.account_id}:image-events/SNSS3NotificationPolicy"
+
+  statement {
+    sid    = "s3-allow-send-messages"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions = [
+      "SNS:Publish",
+    ]
+
+    resources = [
+      "arn:aws:sns:${var.region}:${var.account_id}:image-events",
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+
+      values = [
+        aws_s3_bucket.my-app-images.arn
       ]
     }
   }
