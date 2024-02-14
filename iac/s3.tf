0b099cb9baa4f303479e232f359d0ab7751da9ae
Andre <andrevitorlopes@gmail.com>
2024-02-14 12:08:57 +0100

    Add prefix filter for event notification

--- iac/s3.tf
+++ iac/s3.tf
@@ -6,7 +6,8 @@ resource "aws_s3_bucket_notification" "images_put_notification" {
   bucket = aws_s3_bucket.my-app-images.id
 
   topic {
-    topic_arn = aws_sns_topic.topic.arn
-    events    = ["s3:ObjectCreated:*"]
+    topic_arn     = aws_sns_topic.topic.arn
+    filter_prefix = "images/"
+    events        = ["s3:ObjectCreated:*"]
   }
 }
